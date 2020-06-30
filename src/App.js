0f6035ae4b9f30c0e4353b30d6d74a67a9e9f9dc
Mariusz <31552338+MariuszSzlachta@users.noreply.github.com>
2020-06-30 12:59:30 +0200

    Review App

--- src/App.js
+++ src/App.js
@@ -10,13 +10,16 @@ class App extends React.Component {
   }
 
   componentDidMount() {
+    // functions should be implemented above and bound in constructor or assigned to const
     let getCompanies = async () => {
+      // redundant async
       return fetch('https://recruitment.hal.skygate.io/companies')
         .then(res => {if(!res.ok) {throw Error(res.statusText);} return res.json()})
         .catch(error => alert(error));
     }
     
     let getIncomes = async (_id) => {
+      // redundant async
       return fetch('https://recruitment.hal.skygate.io/incomes/'+_id)
         .then(res => {if(!res.ok) {throw Error(res.statusText);} return res.json()})
         .catch(error => alert(error));
@@ -25,16 +28,20 @@ class App extends React.Component {
     let makeState = async () => {
       let companies = await getCompanies();
       for(let i=0; i<companies.length; i++) { //companies.length
+        // you should use Promise.all, now you are fetching data sequentially
+        // don't use let
         let incomes = await getIncomes(companies[i].id);
         incomes.incomes.forEach(income=>{
           income.date = new Date(income.date)
         })
+        // don't mutate data
         companies[i].incomes = incomes.incomes;
       }
 
       return {companies: companies};
     }
-
+    
+    // you are setting state twice, and you don't understand how react works and it's methods
     this.setState({
       companies: []
     }, () => makeState().then((newState) => this.setState(newState)));
@@ -50,7 +57,10 @@ class App extends React.Component {
   }
 
   render() {
+    
+    // you'd better learn devtools and don't leave comments
     // console.log(this.state);
+    // check content od companies and then return something or loader.
     return (
       <>
         {
